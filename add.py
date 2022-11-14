edf79629cf9c3c9b044e15bb12ce00503a1d955d
Cherry342 <113442276+Cherry342@users.noreply.github.com>
2022-11-14 09:56:15 -0500

    add add.py

---
+++ add.py
@@ -0,0 +1,13 @@
+print("I will add numbers for you :)")
+
+x = int(input("Give me one number")
+y = int(input("Give me one number")
+
+x=int(x)
+y=int(y)
+
+print(x + y)
+
+print("Your result is....")
+
+print=(int(x) + int(y))
