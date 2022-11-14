7c84347cb8d23d84224dcc7b5b2763ffb516032b
Cherry342 <113442276+Cherry342@users.noreply.github.com>
2022-11-14 10:38:47 -0500

    add grades.py

--- grades.py
+++ grades.py
@@ -2,11 +2,12 @@ x=100
 if x < 100:
     
     print("x is a A")
+# Grade must be 80 or higher to pass
 elif x < 80:
     print("x is a B")
 elif x < 70:
     print("x is a C")
-
+# Grade if it's  60 or lower is failure
 else: x <  60 or  50;
 print("x is D or F")
 
