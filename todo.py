0e7073952cf884312147b19aba45709a8ed54d85
Cherry342 <113442276+Cherry342@users.noreply.github.com>
2022-11-15 10:52:29 -0500

    add todo.py

---
+++ todo.py
@@ -0,0 +1,11 @@
+todos=[]
+
+x = input ("What to do add to a  gender reveal party list?")
+
+
+todos.append(x)
+todos=["Party balloons, black balloon (with the right color powderx), cupcakes, blue and pink drinks, crackers"]
+
+print("Your todo list is")
+print(todos)
+
