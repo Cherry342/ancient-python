b50e4af21bc28e575e20d887001af4c3d88600cc
Cherry342 <113442276+Cherry342@users.noreply.github.com>
2022-11-15 10:34:47 -0500

    add loops.py

---
+++ loops.py
@@ -0,0 +1,21 @@
+
+#x =''
+
+# while x !='stop':
+# x = print("hello world")
+# y = input("Type stop to exit")
+
+#y=1
+
+#while y <=10: 
+#     y= y+1 
+# print(y)
+#y+=1
+
+#legendaries= ['moonlight', 'seafairy', 'windarcher', 'firespirit']
+
+#for x in legendaries:
+   # print(x)
+
+for i in range(1,11):
+    print(i)
