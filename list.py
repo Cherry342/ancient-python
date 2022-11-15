a09cd65f9c2bf6b8d8b429e2eb912838aae027f8
Cherry342 <113442276+Cherry342@users.noreply.github.com>
2022-11-15 09:33:04 -0500

    add list.py

---
+++ list.py
@@ -0,0 +1,30 @@
+my_list=[
+    "common",
+    "rare",
+    "epic",
+    "legendary",
+    "ancient"
+]
+print(my_list)
+
+my_list.append('super_epic')
+
+print(my_list)
+
+print(my_list[4])
+
+
+print(my_list[-1])
+
+
+my_tuple=(
+    "purevanilla",
+    "hollyberry",
+    "darkcacao",
+    "goldencheese",
+    "whitelily"
+)
+print(my_tuple)
+
+my_tuple[0]='moonlight'
+#This is a list of ancient type of cookies
