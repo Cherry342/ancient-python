dc7c389924ad93b2f89680ff63c6d8d628959d36
Cherry342 <113442276+Cherry342@users.noreply.github.com>
2022-11-16 10:17:05 -0500

    add functions.py

---
+++ functions.py
@@ -0,0 +1,24 @@
+def hello_world():
+    print('hello_world')
+
+    hello_world()
+    
+    def add(a, b):
+        print(a+b)
+        add( 12,18)
+
+# subtract, multiply, divide, and remainder
+
+#def subtract(c,d):
+   # print(d-c)
+   # subtract(64-49)
+
+#def multiply(e,f):
+    #print(y="mx+b") 
+
+def slope(m,x,b):
+    return(m*x+b)
+
+y=slope(12,18,24)
+
+
