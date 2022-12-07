204413d70d08f3ec024bf62fde39514d0cb24e04
Cherry342 <113442276+Cherry342@users.noreply.github.com>
2022-12-07 10:39:53 -0500

    add try.py

---
+++ try.py
@@ -0,0 +1,40 @@
+try:
+ age = int(input('Enter your age: '))
+
+
+ faveNum = int(input('What is your favorite number: '))
+
+ print("Please input a number")
+
+
+ if age <= 21:
+ print('You are not allowed to enter, you are too young.')
+else:
+ print('Welcome, you are old enough.')
+
+print("Fun Fact! Your age divided by your favorite number is: " , age / faveNum)
+except ValueError:
+print("Please input a number")
+
+except ZeroDivisionError:
+  print("Please  do not enter the number zero")
+
+finally:
+ print("Congrats no ID-10-T errors!")
+
+
+
+
+
+
+
+
+#try:
+    #code that could raise execptions
+#except:
+    #code that will be executed to handle exceptions
+
+#try:
+   # print("Fun Fact! Your age divided by your favorite number is: " , age / faveNum)
+#except ValueError:
+   # print("No number zero please")
