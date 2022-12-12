5c8f42c2b48e5728e635ab5acb1398c319e90597
Cherry342 <113442276+Cherry342@users.noreply.github.com>
2022-12-12 10:28:18 -0500

    add turtles2.py

---
+++ turtles2.py
@@ -0,0 +1,32 @@
+from turtle import Turtle, Screen
+from random import randint, choice as rand_choice
+
+colors=['red','orange','yellow','green','cyan','blue','purple']
+
+class MyTurtle(Turtle):
+    def random_shape(self, x , y):
+        self.color((rand_choice(colors)))
+
+        self.penup()
+        self.setposition(randint(-200,200) , randint(-200, 200))
+        self.pendown()
+        self.circle(50, steps=randint(4,12))
+
+    def __init__(self):
+        super().__init__()
+        self.random_shape(0,0)
+        self.onclick(self.random_shape)
+       # self.begin_fill()
+        #self.circle(randint(10,50),steps=randint(4,12))
+        #self.end_fill()
+x = MyTurtle()
+y = MyTurtle()
+
+x.forward(50)
+y.backward(50)
+input()
+
+
+screen = Screen()
+
+screen.mainloop
